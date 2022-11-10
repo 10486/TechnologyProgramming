b79f4279f31c991e7bafc2d13256c8bccb795a96
Vladislav <v.mezhevov@napoleonit.ru>
2022-11-10 09:55:11 +0500

    fix output lab 1

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,7 +1,8 @@
 ﻿// Lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
 #include <iostream>
+#include <iomanip>
+
 
 double get_c() {
     return (pow(0.027, -1. / 3) - pow((1. / 6), -2.2)) * log(3);
@@ -22,19 +23,42 @@ double get_l() {
 }
 
 double get_y(double x, double a, double b) {
+    if (b == 0) {
+        throw std::runtime_error("divide by zero");
+    }
+    if (x / b < 0) {
+        throw std::runtime_error("sqrt from negative number");
+    }
     return pow(sin(pow(pow(x, 2) + a, 2)), 3) - sqrt(x / b);
 }
 
 double get_z(double x, double a, double b) {
+    if (a == 0) {
+        throw std::runtime_error("divide by zero");
+    }
     return pow(x, 2) / a + cos(pow(x + b, 3));
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
+    int precision;
+    std::cout << "Введите кол-во знаков после запятой: ";
+    std::cin >> precision;
+    std::cout << std::setprecision(precision);
+    std::cout << std::fixed;
+    std::cout << "fixed" << std::endl;
     std::cout << "c = " << get_c() << std::endl;
     std::cout << "k = " << get_k() << std::endl;
     std::cout << "l = " << get_l() << std::endl;
+    std::cout << "scientific" << std::endl;
+    std::cout << std::scientific;
+    std::cout << "c = " << get_c() << std::endl;
+    std::cout << "k = " << get_k() << std::endl;
+    std::cout << "l = " << get_l() << std::endl;
+
+    
+    
 
     double x, a, b;
     std::cout << "Введите x: ";
@@ -43,7 +67,15 @@ int main()
     std::cin >> a;
     std::cout << "Введите b: ";
     std::cin >> b;
-
+    std::cout << "Введите кол-во знаков после запятой: ";
+    std::cin >> precision;
+    std::cout << std::setprecision(precision);
+    std::cout << std::fixed;
+    std::cout << "fixed" << std::endl;
+    std::cout << "y = " << get_y(x, a, b) << std::endl;
+    std::cout << "z = " << get_z(x, a, b) << std::endl;
+    std::cout << "scientific" << std::endl;
+    std::cout << std::scientific;
     std::cout << "y = " << get_y(x, a, b) << std::endl;
     std::cout << "z = " << get_z(x, a, b) << std::endl;
  
